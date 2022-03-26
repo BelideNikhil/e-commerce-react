63fca653e2d6b35a8a83fb87c2ddbd647b22f02f
NikhilBelide <66310861+BelideNikhil@users.noreply.github.com>
2022-03-27 00:40:48 +0530

    feat: cart,wishlist context, fix:auth, ref:barrel

---
+++ src/Context/index.js
@@ -0,0 +1,5 @@
+import { AuthProvider } from "./AuthContext";
+import { CartProvider } from "./CartContext";
+import { WishlistProvider } from "./WishlistContext";
+import { ProductProvider } from "./ProductContext";
+export { AuthProvider, CartProvider, WishlistProvider, ProductProvider };
